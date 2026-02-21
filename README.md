f68dafa4707d3fc91a01c726999d6ba33051e2c4
mikaelvillanen23 <mikael.villanen23@gmail.com>
2026-02-21 23:57:35 +0200

    Finish project
    This commit adds the final touches to playGame(),
    and little cosmetic tweaks here and there.
    
    It also adds some basic UI for starting the game,
    just because I wanted to practise a bit.
    
    Finally, README.md is updated with some thoughts
    after the project.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-This project asks for skills with basic Javascript.
+This project asked for skills with basic Javascript. Guidelines for different functions
+were provided.
+
+-----------------
+Thoughts after finishing the project:
+
+This was fun! Spent maybe a bit too much time tinkering with details here and there, but it was all in the name of learning, and I did learn a good amount!
+
+Lessons learned from this project especially:
+  - '=' does not equal '==='. This is important to remember at all times.
+  - One needs to be aware of what they are logging to the console: otherwise they might
+    spend and hour trying to fix a function that was working perfectly to begin with.
--- script.js
+++ script.js
@@ -14,41 +14,42 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt("Rock, Paper or Scissors? Please type your choice below!");
-  return humanChoice.toLowerCase();
+  let humanChoice = prompt("Rock, Paper or Scissors? Please type your choice below!\n\nThe game will end after 5 rounds are played.");
+  return humanChoice.toLowerCase(); // toLower so that the values will match playRound's conditions
 }
 
 function playRound(computerChoice, humanChoice) {
   switch(true) {
     case (computerChoice === humanChoice):
-      console.log("It's a tie!");
+      console.log("This round is a tie!");
       break;
     case (computerChoice === "paper") && (humanChoice === "rock"):
       computerScore += 1;
-      console.log("You lose! Paper beats Rock.");
+      console.log("You lose this round! Paper beats Rock.");
       break;
     case (computerChoice === "scissors") && (humanChoice === "rock"):
       humanScore += 1;
-      console.log("You win! Rock beats Scissors.");
+      console.log("You win this round! Rock beats Scissors.");
       break;
     case (computerChoice === "scissors") && (humanChoice === "paper"):
       computerScore += 1;
-      console.log("You lose! Scissors beats Paper.");
+      console.log("You lose this round! Scissors beats Paper.");
       break;
     case (computerChoice === "rock") && (humanChoice === "paper"):
       humanScore += 1;
-      console.log("You win! Paper beats Rock.");
+      console.log("You win this round! Paper beats Rock.");
       break;
     case (computerChoice === "rock") && (humanChoice === "scissors"):
       computerScore += 1;
-      console.log("You lose! Rock beats Scissors.");
+      console.log("You lose this round! Rock beats Scissors.");
       break;
     case (computerChoice === "paper") && (humanChoice === "scissors"):
       humanScore += 1;
-      console.log("You win! Scissors beats Paper.");
+      console.log("You win this round! Scissors beats Paper.");
       break;
     default:
-      console.log("To play the game, please type only one of the three options without typos, thank you.");
+      computerScore += 1;
+      console.log("To play the game, please type one of the three options without typos, thank you (case doesn't matter).\n\nSince you didn't do so this time, this round goes to the computer. Tough luck.");
   }
 }
 
@@ -57,27 +58,28 @@ function playGame(numberOfRounds) {
   let humanPlay = getHumanChoice();
 
   while (true) {
-    if (roundsPlayed === (numberOfRounds - 1)) {
+    if (roundsPlayed === (numberOfRounds - 1)) { // start of last round
       playRound(computerPlay, humanPlay);
       if (humanScore === computerScore) {
         console.log("Wow, the game ends in a tie! How boring.");
       } else {
+        console.log("");
         humanScore > computerScore ?
           console.log(`You win the game ${humanScore} - ${computerScore}!`) :
           console.log(`Computer wins the game ${humanScore} - ${computerScore}!`);
       }
       console.log("");
-      console.log("To start a new game, refresh this page.")
+      console.log("To clear the console and start a new game, refresh this page.\nClicking 'Start game' again will break the logic of this program.")
       break;
     } else {
       playRound(computerPlay, humanPlay);
       roundsPlayed += 1;
-      console.log(`Current score is: YOU ${humanScore} | Computer ${computerScore}.`);
+      console.log(`Current score is: You ${humanScore} | Computer ${computerScore}`);
       console.log("");
-      computerPlay = getComputerChoice();
+      computerPlay = getComputerChoice(); // variables updated with a new function call to get new choices for the next round. Without the call, the choices will remain unchanged for the whole game.
       humanPlay = getHumanChoice();
     }
   }
 }
 
-playGame(5);
+// playGame(5);
